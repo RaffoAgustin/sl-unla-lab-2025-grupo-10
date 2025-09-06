3e790af76cc9ac84ba7e6efc6ff8b35540220eb4
thiagoseoane <149105659+thiagoseoane@users.noreply.github.com>
2025-09-06 14:16:02 -0300

    agregar endpoint para eliminar turno (#9)
    * definir clases Persona y Turno y configurar la base de datos
    
    * eliminar una persona
    
    * eliminar un turno

---
+++ eliminar_turno.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, HTTPException, status
+from models import session, Turno
+
+app = FastAPI()
+
+# Eliminar un turno
+@app.delete("/turnos/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def eliminar_turno(id: int):
+ turno = Turno.query.get(id) 
+ if turno is None:
+     raise HTTPException(status_code=404, detail="Turno no encontrado")
+ session.delete(turno)
+ session.commit()
+ return {"detail": "Turno eliminado"}
