6437617e1c0a83d5edd3971883a8c6eb24668c54
Ignacio David Vega <ignaciodavidvega@outlook.com>
2025-09-15 11:48:16 -0300

    Actualizar comentario en obtener_persona.py para mayor claridad y modificaciones en el código.

--- obtener_persona.py
+++ obtener_persona.py
@@ -1,13 +1,14 @@
+#Codigo para mostrar una sola persona de la base de datos
+
 from fastapi import FastAPI, HTTPException, Depends
 from models import Persona
-from database import session
+from database import get_db
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-#Codigo para mostrar una sola persona
-
 @app.get("/personas/{id}")
-def obtener_persona(id: int, db=Depends(lambda: session)):
+def obtener_persona(id: int, db: Session = Depends(get_db)):
     persona = db.get(Persona, id)
     if persona is None:
         raise HTTPException(status_code=404, detail="La persona no fue encontrada en la base de datos")
