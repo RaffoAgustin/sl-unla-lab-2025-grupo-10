5d62d5ca4043cfb1a99534586927bd772d6a089c
thiagoseoane <thiagonseoane@gmail.com>
2025-09-14 21:04:13 -0300

    modifico el archivo luego de haber creado la dependencia

--- eliminar_persona.py
+++ eliminar_persona.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from models import Persona
-from database import session
+from sqlalchemy.orm import Session
+from database import get_db
+
 app = FastAPI()
 
 # Eliminar una persona
 @app.delete("/personas/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def eliminar_persona(id: int):
+def eliminar_persona(id: int, db: Session = Depends(get_db)):
     try:
-        persona = session.get(Persona, id)
+        persona = db.get(Persona, id)
         if persona is None:
             raise HTTPException(status_code=404, detail="Persona no encontrada")
-        session.delete(persona)
-        session.commit()
+        db.delete(persona)
+        db.commit()
     except Exception as e:
-        session.rollback()
+        db.rollback()
         raise HTTPException(status_code=500, detail="Error al eliminar la persona")
