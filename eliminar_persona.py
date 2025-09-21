283c5e7e8e64b16d40d97f163129d07815d2f94b
thiagoseoane <thiagonseoane@gmail.com>
2025-09-21 19:53:54 -0300

    cambios

--- eliminar_persona.py
+++ eliminar_persona.py
@@ -8,13 +8,14 @@ router = APIRouter()
 # Eliminar una persona
 @router.delete("/personas/{id}", status_code=status.HTTP_200_OK)
 def eliminar_persona(id: int, db: Session = Depends(get_db)):
-    try:
         persona = db.get(Persona, id)
         if persona is None:
             raise HTTPException(status_code=404, detail=f"Persona con ID {id} no encontrada")
-        db.delete(persona)
-        db.commit()
-        return {"mensaje": f"La persona con ID {id} se eliminó correctamente"}
-    except Exception as e:
-        db.rollback()
-        raise HTTPException(status_code=500, detail=f"Error al eliminar la persona con ID {id}")
+        try:
+            db.delete(persona)
+            db.commit()
+            return {"mensaje": f"La persona con ID {id} se eliminó correctamente"}
+        
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(status_code=500, detail=f"Error al eliminar la persona con ID {id}")
