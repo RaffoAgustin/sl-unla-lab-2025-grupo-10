6593b00289c689aaae5a4af5f59312a81e92ab2c
thiagoseoane <thiagonseoane@gmail.com>
2025-09-21 19:52:47 -0300

    cambios

--- eliminar_turno.py
+++ eliminar_turno.py
@@ -7,20 +7,20 @@ router = APIRouter()
 
 @router.delete("/turnos/{id}", status_code=status.HTTP_200_OK)
 def eliminar_turno(id: int, db: Session = Depends(get_db)):
-    try:
         turno = db.get(Turno, id)
         if turno is None:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail=f"Turno con ID {id} no encontrado"
         )
-
-        db.delete(turno)
-        db.commit()
-        return {"mensaje": f"Turno con ID {id} eliminado correctamente"}
-    except Exception as e:
-        db.rollback()
-        raise HTTPException(
+        try:
+            db.delete(turno)
+            db.commit()
+            return {"mensaje": f"Turno con ID {id} eliminado correctamente"}
+        
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Error al eliminar el turno"
         )
