160eb7cd01e5ec415dae4fbb7600ee3b3e6eb1d3
Ignacio David Vega <ignaciodavidvega@outlook.com>
2025-09-21 15:58:53 -0300

    Se añade el endpoint para listar turnos con detalles de la persona

---
+++ listado_turnos.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, HTTPException, Depends
+from models import Persona, Turno
+from database import get_db
+from sqlalchemy.orm import Session
+from database import get_db
+
+router = APIRouter()
+
+# Listado de los turnos en la BD
+@router.get("/turnos")
+def listado_turnos(db: Session = Depends(get_db)):
+    turnos = db.query(Turno).all()
+    if not turnos:
+        raise HTTPException(status_code=404, detail="No se han encontrado turnos en la base de datos")
+
+    resultado = []  # Lista para almacenar los turnos con detalles de la persona
+    for turno in turnos:
+        persona = db.query(Persona).filter(Persona.id == turno.persona_id).first()
+        turno_info = {
+            "ID Turno": turno.id,
+            "Fecha": turno.fecha,
+            "Hora": str(turno.hora),
+            "Estado": turno.estado,
+            "Persona": {
+                "ID Persona": persona.id,
+                "Nombre": persona.nombre,
+                "Apellido": persona.apellido,
+                "Email": persona.email
+            } if persona else None
+        }
+        resultado.append(turno_info)
+    
+    return resultado
