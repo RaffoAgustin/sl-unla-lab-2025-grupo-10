168f1021b0e21fd4c3b33b964aa412c71746e65b
Thiago Nahuel Seoane <149105659+thiagoseoane@users.noreply.github.com>
2025-10-06 16:05:10 -0300

    Se agregó un endpoint GET /reportes/turnos-por-fecha que permite obtener todos los turnos correspondientes a una fecha determinada, incluyendo los datos de la persona asociada.  - Se usa join con Persona. - Devuelve lista de turnos o mensaje si no hay resultados. - Manejo de errores mejorado. (#19)
    * agrego la db
    
    * Actualizo el README con las nuevas funcionalidades para el hito 2
    
    * Agrego endpoint para obtener turnos por fecha

---
+++ Turnos/turnos_de_una_fecha.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, HTTPException, Query, status
+from sqlalchemy.orm import Session
+from DataBase.models import Turno, Persona
+from DataBase.database import get_db
+
+from datetime import date
+
+router = APIRouter()
+
+# Obtener los turnos de una fecha
+@router.get("reportes/turnos-por-fecha")
+def obtener_turnos_de_una_fecha(
+    fecha: date = Query(..., description="Fecha en formato YYYY-MM-DD"), #Pide al usuario un elemento date (... significa obligatorio)
+    db: Session = Depends(get_db)  #Inyecta automáticamente una sesión de base de datos
+):
+    try:
+        turnos = db.query(Turno).join(Persona).filter(Turno.fecha == fecha).all()
+        
+        # Si no hay turnos en esa fecha
+        if not turnos:
+            return {"mensaje": f"No se encontraron turnos para la fecha {fecha}"}
+        
+        return [
+        {
+            "Fecha": t.fecha,
+            "Hora": t.hora,
+            "Estado": t.estado,
+            "Persona": {
+                "nombre": t.persona.nombre,
+                "dni": t.persona.dni,
+            } if t.persona else None     
+        } for t in turnos
+    ]
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al obtener los turnos de la fecha {fecha}: {str(e)}")
