e91420a44a5ca8fbc0a2cc5963874f37d3475477
thiagoseoane <thiagonseoane@gmail.com>
2025-10-13 17:03:50 -0300

    marco turnos como cancelados usando PATCH (eliminación lógica)

---
+++ Turnos/eliminar_turno_logicamente.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.orm import Session
+from DataBase.models import Turno
+from DataBase.database import get_db
+
+router = APIRouter()
+
+@router.patch("/turnos/{id}", status_code=status.HTTP_200_OK)
+def eliminar_turno_logicamente(id: int, db: Session = Depends(get_db)):
+        turno = db.get(Turno, id)
+        if turno is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Turno con ID {id} no encontrado"
+        )
+        try:
+            turno.estado = "cancelado"
+            db.commit()
+            return {"mensaje": f"Turno con ID {id} eliminado correctamente"}
+        
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error al eliminar el turno"
+        )
