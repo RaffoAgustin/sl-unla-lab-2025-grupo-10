01fd66dec7f5fa07f4bf2c6e5c61c23f0dafa617
thiagoseoane <thiagonseoane@gmail.com>
2025-10-13 17:09:22 -0300

    agrego la variable de máximo de turnos cancelados por si cambia en el futuro

--- variables.py
+++ variables.py
@@ -8,5 +8,7 @@ ESTADO_TURNO = ["Pendiente", "Cancelado", "Confirmado", "Asistido"]
 HORARIOS_VALIDOS = [
     time(9,0), time(9,30), time(10,0), time(10,30), time(11,0), time(11,30),
     time(12,0), time(12,30), time(13,0), time(13,30), time(14,0), time(14,30),
-    time(15,0), time(15,30), time(16,0), time(16,30), time(17,0)
+    time(15,0), time(15,30), time(16,0), time(16,30)
     ]
+
+MAX_CANCELADOS = 5
