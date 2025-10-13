1151ffdc09710ce163cb033c937e3268dba219c5
thiagoseoane <thiagonseoane@gmail.com>
2025-10-13 16:10:27 -0300

    agregar propiedad edad a Persona usando calcular_edad de utils

--- DataBase/models.py
+++ DataBase/models.py
@@ -1,19 +1,22 @@
 from sqlalchemy import Column, Integer, String, Boolean, Date, Time, ForeignKey
 from sqlalchemy.orm import relationship
 from DataBase.database import Base
-
+from utils import calcular_edad
 
 class Persona(Base):
     __tablename__ = "personas"
 
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
     dni = Column(String, unique=True, nullable=False)
-    telefono = Column(String, nullable=False)
+    telefono = Column(String, unique=True, nullable=False)
     fecha_nacimiento = Column(Date, nullable=False)
     esta_habilitado = Column(Boolean, default=True, nullable=False)
-
+    
+    @property
+    def edad(self):
+        return calcular_edad(self.fecha_nacimiento)
 
 class Turno(Base):
     __tablename__ = "turnos"
