34ba93046cc1a346961a4dbaec37ec7969ef92ac
PonceMateo <Poncemateo2712@gmail.com>
2025-10-14 22:07:50 -0300

    Agregar la paginación a través de un nuevo parámetro en el endpoint

--- Turnos/turnos_confirmados_periodo.py
+++ Turnos/turnos_confirmados_periodo.py
@@ -11,9 +11,14 @@ router = APIRouter()
 def turnos_confirmados_periodo(
     desde: date = Query(..., description="Fecha en formato YYYY-MM-DD"), #Consulta obligatoria de la fecha inicial
     hasta: Optional[date] = Query(None, description="Fecha en formato YYYY-MM-DD"), #Consulta opcional de la fecha límite
+    pag: Optional[int] = Query(1, ge=1, description="Numero de pagina (minimo 1)"), #El predeterminado es 1 aunque no se ponga. ge=1 significa que debe ser mayor o igual a 1
     db: Session = Depends(get_db)  #Inyecta automáticamente una sesión de base de datos
 ):
-    if (hasta != None) and (hasta < desde): #Si "hasta" fue indicado, verifica que sea mayor a "desde"
+    ## Ejemplo de Endpoint: 
+    ## /reportes/turnos-confirmados?desde=2024-02-17&hasta=2025-12-24&pag=3
+
+
+    if hasta and (hasta < desde): #Si "hasta" fue indicado, verifica que sea mayor a "desde"
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="La fecha 'hasta' no puede ser anterior a la fecha 'desde'"
@@ -31,12 +36,23 @@ def turnos_confirmados_periodo(
         if hasta: #Si "hasta" fue indicado...
             turnosConfirmados = turnosConfirmados.filter(Turno.fecha <= hasta) #...También filtro por las fechas más viejas que "hasta"
 
-        #TODO: Meter la paginación
-        #No se si hay que indicar en la query del endpoint al número de página, 
-        #o simplemente devolver el json completo dividido entre 5 elementos.
+        #TODO: La variable "cantElementosXPagina" debería ser una variable de entorno, quizas.
+        cantElementosXPagina = 5
+
+        paginaDeTurnos = (
+            turnosConfirmados
+            .order_by(Turno.fecha) #Ordeno por fecha de mas antiguo a más reciente
+            .offset(cantElementosXPagina * (pag-1)) #Offset es la cantidad de elementos salteados antes de mostrar.
+            .limit(cantElementosXPagina) #Limito la cantidad de registros por su variable
+            .all() #Muestra todos los registros bajo estos parámetros
+        )
 
 
-        return turnosConfirmados
+        return {
+            "pagina": pag,
+            "cantElementos": cantElementosXPagina,
+            "turnos": paginaDeTurnos,
+        }
     
     except Exception as e:
         raise HTTPException(
