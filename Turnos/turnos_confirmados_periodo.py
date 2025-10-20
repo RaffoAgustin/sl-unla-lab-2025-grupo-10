9c0e451262c6fd5276f667b98be500b03ff54ced
PonceMateo <Poncemateo2712@gmail.com>
2025-10-20 15:57:41 -0300

    Utilizar FechaQuery en el Endpoint de turnosConfirmadosPeriodo

--- Turnos/turnos_confirmados_periodo.py
+++ Turnos/turnos_confirmados_periodo.py
@@ -5,36 +5,45 @@ from typing import Optional
 from Utils.config import ESTADOS_TURNO
 from DataBase.models import Turno
 from DataBase.database import get_db
+from schemas import FechaQuery
 
 router = APIRouter()
 
 @router.get("/reportes/turnos-confirmados")
 def turnos_confirmados_periodo(
-    desde: date = Query(..., description="Fecha en formato YYYY-MM-DD"), #Consulta obligatoria de la fecha inicial
-    hasta: Optional[date] = Query(None, description="Fecha en formato YYYY-MM-DD"), #Consulta opcional de la fecha límite
+    desde: str = Query(..., description="Fecha futura en formato DD-MM-YYYY"), #Consulta obligatoria de la fecha inicial (Recibo un string)
+    hasta: Optional[str] = Query(None, description="Fecha futura en formato DD-MM-YYYY"),  #Consulta opcional de la fecha límite
     pag: Optional[int] = Query(1, ge=1, description="Numero de pagina (minimo 1)"), #El predeterminado es 1 aunque no se ponga. ge=1 significa que debe ser mayor o igual a 1
     db: Session = Depends(get_db)  #Inyecta automáticamente una sesión de base de datos
 ):
     ## Ejemplo de Endpoint: 
-    ## /reportes/turnos-confirmados?desde=2024-02-17&hasta=2025-12-24&pag=3
+    ## /reportes/turnos-confirmados?desde=20-10-2025&hasta=30-12-2025&pag=3
+
     try:
-        
-        if hasta and (hasta < desde): #Si "hasta" fue indicado, verifica que sea mayor a "desde"
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="La fecha 'hasta' no puede ser anterior a la fecha 'desde'"
-            )
+        # Convierto el string "desde" a un objeto date a través de la clase FechaQuery y sus validaciones
+        fecha_desde = FechaQuery(fecha=desde).fecha 
+                                #     ^ Recibe a "desde" como parámetro
+
+        # Validar fecha hasta si existe
+        fecha_hasta = None #Inicializo la variable
+        if hasta:
+            fecha_hasta = FechaQuery(fecha=hasta).fecha #Hago lo mismo que con "desde"
+            if fecha_hasta < fecha_desde:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="La fecha 'hasta' no puede ser anterior a la fecha 'desde'"
+                )
 
         #Consulta inicial
         turnosConfirmados = (
             db.query(Turno)
             .filter(
                 Turno.estado == ESTADOS_TURNO[2], #Aquellos turnos confirmados
-                Turno.fecha >= desde) #Donde su fecha sea mas reciente que "desde"
+                Turno.fecha >= fecha_desde) #Donde su fecha sea mas reciente que "desde"
                 )
         
         if hasta: #Si "hasta" fue indicado...
-            turnosConfirmados = turnosConfirmados.filter(Turno.fecha <= hasta) #...También filtro por las fechas más viejas que "hasta"
+            turnosConfirmados = turnosConfirmados.filter(Turno.fecha <= fecha_hasta) #...También filtro por las fechas más viejas que "hasta"
 
         #TODO: La variable "cantElementosXPagina" debería ser una variable de entorno, quizas.
         cantElementosXPagina = 5
