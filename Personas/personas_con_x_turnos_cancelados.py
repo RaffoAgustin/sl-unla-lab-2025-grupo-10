3aa6d668227b0beb38ef51b080758d4f602e10e6
PonceMateo <Poncemateo2712@gmail.com>
2025-11-30 20:19:42 -0300

    Meter en el try la validación y cambiarla para que "min" no pueda dar 0

--- Personas/personas_con_x_turnos_cancelados.py
+++ Personas/personas_con_x_turnos_cancelados.py
@@ -12,24 +12,24 @@ def personas_con_turnos_cancelados(
     min: int = Query(..., description="Minimo de turnos cancelados"), #Pide al usuario el mínimo de Turnos cancelados (... significa obligatorio)
     db: Session = Depends(get_db)  #Inyecta automáticamente una sesión de base de datos
 ):
-    if min < 0:
-            raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="El valor minimo no puede ser negativo"
-     )
-    
     try:
-        personas=(
-            db.query(Persona) #Hace una consulta de personas
-               .join(Turno) #Junto a sus turnos
-               .filter(Turno.estado == ESTADOS_TURNO[1]) #Tomo unicamente a los turnos cancelados
-               .group_by(Persona.id) #Agrupo a las personas según su id, necesario para que las func funcionen correctamente
-               .having(func.count(Turno.id) >= min) #Filtro los grupos (cada persona) donde la cantidad de turnos (ya filtrados) sean mayores al minimo
-               .all() #Muestro todas las personas con estas condiciones
+        if min <= 0:
+                raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="El valor minimo no puede ser negativo o 0"
         )
+        
+        personas=(
+                db.query(Persona) #Hace una consulta de personas
+                .join(Turno) #Junto a sus turnos
+                .filter(Turno.estado == ESTADOS_TURNO[1]) #Tomo unicamente a los turnos cancelados
+                .group_by(Persona.id) #Agrupo a las personas según su id, necesario para que las func funcionen correctamente
+                .having(func.count(Turno.id) >= min) #Filtro los grupos (cada persona) donde la cantidad de turnos (ya filtrados) sean mayores al minimo
+                .all() #Muestro todas las personas con estas condiciones
+            )
 
         return personas
-    
+        
     except Exception as e:
         print("Error en el reporte de turnos cancelados:", e)
         raise HTTPException(
