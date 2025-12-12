864b4e080ff3492a7b69a3413de6e27c1b759616
PonceMateo <Poncemateo2712@gmail.com>
2025-12-12 13:13:21 -0300

    Cambio de nombre de la variable "min" a "min_turnos" para que no se pise con la función integrada "min()"

--- Reportes/CSV/personas_con_x_turnos_cancelados_csv.py
+++ Reportes/CSV/personas_con_x_turnos_cancelados_csv.py
@@ -13,11 +13,11 @@ router = APIRouter()
 
 @router.get("/csv/turnos-cancelados-minimo")
 def exportar_personas_con_turnos_cancelados_csv(
-    min: int = Query(..., description="Minimo de turnos cancelados"), #Pide al usuario el mínimo de Turnos cancelados (... significa obligatorio)
+    min_turnos: int = Query(..., description="Minimo de turnos cancelados"), #Pide al usuario el mínimo de Turnos cancelados (... significa obligatorio)
     db: Session = Depends(get_db)  #Inyecta automáticamente una sesión de base de datos
 ):
     try:
-        if min <= 0:
+        if min_turnos <= 0:
                 raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="El valor minimo no puede ser negativo o 0"
@@ -28,12 +28,12 @@ def exportar_personas_con_turnos_cancelados_csv(
                 .join(Turno) #Junto a sus turnos
                 .filter(Turno.estado == ESTADOS_TURNO.Cancelado) #Tomo unicamente a los turnos cancelados
                 .group_by(Persona.id) #Agrupo a las personas según su id, necesario para que las func funcionen correctamente
-                .having(func.count(Turno.id) >= min) #Filtro los grupos (cada persona) donde la cantidad de turnos (ya filtrados) sean mayores al minimo
+                .having(func.count(Turno.id) >= min_turnos) #Filtro los grupos (cada persona) donde la cantidad de turnos (ya filtrados) sean mayores al minimo
                 .all() #Muestro todas las personas con estas condiciones
             )
 
         if not personas:
-            return {"mensaje": f"No se encontraron personas con mas de {min} turnos cancelados"}
+            return {"mensaje": f"No se encontraron personas con mas de {min_turnos} turnos cancelados"}
         
         filas = []
 
@@ -59,7 +59,7 @@ def exportar_personas_con_turnos_cancelados_csv(
         return StreamingResponse(
             buffer,
             media_type="text/csv",
-            headers={"Content-Disposition": f"attachment; filename=turnos_cancelados_min_{min}.csv"}
+            headers={"Content-Disposition": f"attachment; filename=turnos_cancelados_min_{min_turnos}.csv"}
         )
     
     except Exception as e:
